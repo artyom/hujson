29d02f17db3cd7b2670404279873848d796c4af9
Artyom Pervukhin <github@artyom.dev>
2022-05-07 14:29:58 +0300

    Add -d flag to print diffs
    Diff code copied from the Go source tree (src/internal/diff), introduced by
    commit 7747c33a41491be74da65b116718f4df7a2f8337.

--- main.go
+++ main.go
@@ -12,33 +12,51 @@ import (
 	"log"
 	"os"
 
+	"github.com/artyom/hujson/internal/diff"
 	"github.com/tailscale/hujson"
 )
 
 func main() {
 	log.SetFlags(0)
-	var replace bool
-	flag.BoolVar(&replace, "w", replace,
-		"overwrite sources instead of printing (re)formatted text")
+	var args runArgs
+	flag.BoolVar(&args.replace, "w", args.replace,
+		"overwrite sources instead of printing formatted text")
+	flag.BoolVar(&args.diff, "d", args.diff,
+		"display diffs instead of printing formatted text")
 	flag.Parse()
-	if err := run(replace, flag.Args()...); err != nil {
+	if err := run(args, flag.Args()...); err != nil {
+		if errors.Is(err, errNonEmptyDiff) {
+			os.Exit(1)
+		}
 		log.Fatal(err)
 	}
 }
 
-func run(replace bool, names ...string) error {
+type runArgs struct {
+	replace, diff bool
+}
+
+func run(args runArgs, names ...string) error {
 	if len(names) == 0 {
 		return errors.New("nothing to do")
 	}
+	var hasDiffs bool
 	for _, name := range names {
-		if err := formatFile(replace, name); err != nil {
+		if err := formatFile(args, name); err != nil {
+			if errors.Is(err, errNonEmptyDiff) {
+				hasDiffs = true
+				continue
+			}
 			return fmt.Errorf("%s: %w", name, err)
 		}
+	}
+	if hasDiffs {
+		return errNonEmptyDiff
 	}
 	return nil
 }
 
-func formatFile(replace bool, name string) error {
+func formatFile(args runArgs, name string) error {
 	b, err := os.ReadFile(name)
 	if err != nil {
 		return err
@@ -48,7 +66,14 @@ func formatFile(replace bool, name string) error {
 		return err
 	}
 	val.Format()
-	if !replace {
+	if args.diff {
+		if res := diff.Diff(name, b, name+".new", []byte(val.String())); res != nil {
+			fmt.Printf("%s", res)
+			return errNonEmptyDiff
+		}
+		return nil
+	}
+	if !args.replace {
 		fmt.Print(val.String())
 		return nil
 	}
@@ -61,3 +86,5 @@ func init() {
 		flag.PrintDefaults()
 	}
 }
+
+var errNonEmptyDiff = errors.New("non-empty diff")
